5bf58a7129b085609a8f06468f632c1b38dc540e
George Zigkas <ziggasg@hotmail.com>
2017-04-23 17:25:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,4 +21,5 @@ if ($hub_verify_token === $verify_token) {
 
 $input = json_decode(file_get_contents('php://input'), true);
 file_get_contents('http://onleave.online/assets/php/v1/chatBot?context='.json_encode($input));
+echo json_encode($input);
 ?>
