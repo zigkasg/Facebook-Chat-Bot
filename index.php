59927c39bdbbbc4778e9c66f4395fe3e58556c92
George Zigkas <ziggasg@hotmail.com>
2017-07-28 18:49:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 $access_token = "EAADU14lUe40BAEYkkTZAYab2ZBQOAms8EkItK1ZCaeXSyw8z9kaljxp4F1vT7KiAZBRsMkRVoAWpZAZB3JyTrtB9gN0k0lbSsXuEOtVjRnjIt4Tsjfb2OthtjfvCbBf15yNnDNWdAevpn0aV0ZC8q2fdctx0ScucczCPseoS2C8MwZDZD";
 $verify_token = "test";
 $hub_verify_token = null;
-
+ini_set('always_populate_raw_post_data', '-1');
 if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
     $hub_verify_token = $_REQUEST['hub_verify_token'];
