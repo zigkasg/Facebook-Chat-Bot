e06b4255478e020619514f0f407d9a16b66358f6
George Zigkas <ziggasg@hotmail.com>
2017-07-31 17:52:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,22 @@ if ($hub_verify_token === $verify_token) {
     return;
 }
 
-$input = json_decode(file_get_contents('php://input'), true);
-file_get_contents('https://www.onleave.online/assets/php/v1/chatBot?context='.json_encode($input));
-echo json_encode($input);
+//$input = json_decode(file_get_contents('php://input'), true);
+//file_get_contents('https://www.onleave.online/assets/php/v1/chatBot?context='.json_encode($input));
+$url = 'https://www.onleave.online/assets/php/v1/chatBotWebhook';
+$data = json_encode($input);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
 ?>
