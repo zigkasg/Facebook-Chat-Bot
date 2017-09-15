3f4467e5e1abb9a5d6fba3ce75b82ea158453199
George Zigkas <ziggasg@hotmail.com>
2017-09-15 12:22:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Date: 24/04/16
  * Time: 3:26 PM
  */
-$access_token = "EAADU14lUe40BAEYkkTZAYab2ZBQOAms8EkItK1ZCaeXSyw8z9kaljxp4F1vT7KiAZBRsMkRVoAWpZAZB3JyTrtB9gN0k0lbSsXuEOtVjRnjIt4Tsjfb2OthtjfvCbBf15yNnDNWdAevpn0aV0ZC8q2fdctx0ScucczCPseoS2C8MwZDZD";
+$access_token = "";
 $verify_token = "test";
 $hub_verify_token = null;
 ini_set('always_populate_raw_post_data', '-1');
